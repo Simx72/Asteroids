381f5d909b95b6a5e1865f24064ddfdcbc9e5e60
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-05 17:42:05 -0500

    add to project

--- ts/functions/disparo.ts
+++ ts/functions/disparo.ts
@@ -12,7 +12,7 @@ export default function disparo(this: AsteroidsMainScene) {
 
   disp.setAngle(nave.angle - 90)
 
-  disp.body.setCircle(100, 0, 0)
+  disp.body.setCircle(800, 0, 0)
 
   disp.body.angle = Phaser.Math.DegToRad(disp.angle)
 
