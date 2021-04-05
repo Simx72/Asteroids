16b2f6987a999bee648953d2b6e08bf897405078
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-05 16:27:45 -0500

    add to project

--- ts/functions/create-nave.ts
+++ ts/functions/create-nave.ts
@@ -52,7 +52,7 @@ export default function createNave(this: AsteroidsMainScene) {
   let disparos = this.objeto(
     'grupo.disparos',
     this.physics.add.group([], {
-      defaultKey: 'bola de fuego'
+      defaultKey: 'fueguito'
     })
   )
 
