a1a364ccd8d5a428c3f3832fcefa2eb512a041e6
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-05 17:31:08 -0500

    add to project

--- ts/functions/create-asteroides.ts
+++ ts/functions/create-asteroides.ts
@@ -56,6 +56,7 @@ export default function createAsteroides(this: AsteroidsMainScene) {
 
       exp
         .setVisible(true)
+        .on('animationcomplete', () => exp.setVisible(false))
         .anims.play('explotar')
 
       let audio = this.sound.add('audio.explo')
