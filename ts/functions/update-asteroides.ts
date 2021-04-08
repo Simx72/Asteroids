00da95e3eaa697abc289fbd96ece991a180f1f0d
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-08 13:53:42 -0500

    add to project

--- ts/functions/update-asteroides.ts
+++ ts/functions/update-asteroides.ts
@@ -1,4 +1,6 @@
 import AsteroidsMainScene from '../scenes/game-scene';
 export function updateAsteroides(this: AsteroidsMainScene) {
-  
+  let asteroides = <Phaser.GameObjects.Group>this.objeto('grupo.ast')
+
+  asteroides.rotate(Phaser.Math.DegToRad(3))
 }
