a26a9e9fea00d8708d26b702887757a9b363d9ab
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-08 19:22:55 -0500

    add to project

--- ts/functions/update-asteroides.ts
+++ ts/functions/update-asteroides.ts
@@ -9,18 +9,34 @@ export function updateAsteroides(this: AsteroidsMainScene) {
       y = 0,
       angulo = 0,
       vel = 0;
-    
+
     let asteroide = asteroides.create(x, y) as Phaser.Physics.Arcade.Image
 
     let texture = ''
 
-    switch (Phaser.Math.Between(1, 3)) {
+    switch (Phaser.Math.Between(1, 2)) {
       case 1: texture = 'asteroide.1'; break;
       case 2: texture = 'asteroide.2'; break;
       case 3: texture = 'asteroide.3'; break;
     }
+    
+    asteroide.setTexture(texture).setScale(0.12, 0.12)
 
-    asteroide.setTexture(texture)
+    if (asteroide.texture.key == 'asteroide.1')
+      asteroide.body.setCircle(
+        asteroide.height * 0.45,
+        asteroide.height * 0.05,
+        asteroide.height * 0.05
+      )
+    if (asteroide.texture.key == 'asteroide.2')
+      asteroide.body.setCircle(
+        asteroide.height * 0.32,
+        asteroide.height * 0.18,
+        asteroide.height * 0.18
+      )
+
+    asteroide.body.pushable = false
+
 
     this.scene.pause()
 
