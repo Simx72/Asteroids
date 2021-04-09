2f221b212027250e63afa6d31b67509eee8fc7d9
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-08 20:03:55 -0500

    add to project

--- ts/functions/update-asteroides.ts
+++ ts/functions/update-asteroides.ts
@@ -3,6 +3,10 @@ export function updateAsteroides(this: AsteroidsMainScene) {
   let asteroides = <Phaser.Physics.Arcade.Group>this.objeto('grupo.ast')
 
   asteroides.rotate(Phaser.Math.DegToRad(3))
+  asteroides.children.each(go => {
+    let ast = go as Phaser.Physics.Arcade.Image
+    if ((-ast.displayWidth / 2) > ast.x && (ast.displayWidth / 2 + this.scale.width) < ast.x && (-ast.displayHeight / 2) > ast.y && (ast.displayHeight/2 + this.scale.height) < ast.y) ast.destroy
+  })
 
   if (this.game.getFrame() % 3 == 0 && this.dato('cargado')) {
 
