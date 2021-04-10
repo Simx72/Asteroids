415c6d85b1b8a58d92fd279461d8e53b929d4063
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-10 14:01:01 -0500

    add to project

--- ts/functions/animateCSS.ts
+++ ts/functions/animateCSS.ts
@@ -1,6 +1,6 @@
-const animateCSS = (element: string, animation: string, prefix = 'animate__') =>
+export function animateCSS(element: string, animation: string, prefix = 'animate__') {
   // We create a Promise and return it
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, _reject) => {
     const animationName = `${prefix}${animation}`;
     const node = document.querySelector(element);
 
@@ -12,7 +12,8 @@ const animateCSS = (element: string, animation: string, prefix = 'animate__') =>
       node.addEventListener('animationend', (event) => {
         event.stopPropagation();
         node.classList.remove(`${prefix}animated`, animationName);
-        resolve('Animation ended');
+        resolve(void 0);
       }, { once: true });
     }
   });
+}
