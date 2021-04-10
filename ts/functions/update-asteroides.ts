938eb29c1e55ca6902afd852a7e0cd97ffee63e3
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-10 09:12:35 -0500

    add to project

--- ts/functions/update-asteroides.ts
+++ ts/functions/update-asteroides.ts
@@ -5,14 +5,18 @@ export function updateAsteroides(this: AsteroidsMainScene) {
   asteroides.rotate(Phaser.Math.DegToRad(3))
   asteroides.children.each(go => {
     let ast = go as Phaser.Physics.Arcade.Image
-    if ((-ast.displayWidth / 2) > ast.x && (ast.displayWidth / 2 + this.scale.width) < ast.x && (-ast.displayHeight / 2) > ast.y && (ast.displayHeight/2 + this.scale.height) < ast.y) ast.destroy()
+    if ((-ast.displayWidth / 2) > ast.x && (ast.displayWidth / 2 + this.scale.width) < ast.x && (-ast.displayHeight / 2) > ast.y && (ast.displayHeight / 2 + this.scale.height) < ast.y) ast.destroy()
   })
 
-  if (this.game.getFrame() % 2 == 0 && this.dato('cargado')) { }
+  if (!this.physics.config.debug) {
+    if (this.game.getFrame() % 2 == 0 && this.dato('cargado')) {
+      nuevoAsteroide.bind(this)()
+    }
+  }
 
 }
 
-function nuevoAsteroide(this: AsteroidsMainScene) {
+export function nuevoAsteroide(this: AsteroidsMainScene) {
   let side = Phaser.Math.Between(1, 4)
   let x = 0, y = 0, angulo = 0, vel = 50;
 
