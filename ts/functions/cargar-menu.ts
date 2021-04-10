25392fbbe79f4499ac0b65db147fd98dc7917fac
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-10 13:05:23 -0500

    add to project

--- ts/functions/cargar-menu.ts
+++ ts/functions/cargar-menu.ts
@@ -2,12 +2,16 @@ import AsteroidsScene from "../scenes/templates/asteroids-scene";
 export function actualizarNivel(this: AsteroidsScene) {
   let puntos = this.dato('puntos')
   if (puntos < 100) {
+    this.dato('nivel', 1)
     document.body.className = "level-1"
   } else if (puntos < 200) {
+    this.dato('nivel', 2)
     document.body.className = "level-2"
   } else if (puntos < 300) {
+    this.dato('nivel', 3)
     document.body.className = "level-3"
   } else if (puntos < 400) {
+    this.dato('nivel', 4)
     document.body.className = "level-4"
   }
 }
