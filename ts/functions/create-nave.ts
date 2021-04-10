347d137d9d7dd51c4d7e04a4b6ecfbbbb1eaea62
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-10 16:38:47 -0500

    add to project

--- ts/functions/create-nave.ts
+++ ts/functions/create-nave.ts
@@ -26,7 +26,8 @@ export default function createNave(this: AsteroidsMainScene) {
     "setXY.stepX": nave.displayWidth + 2,
     key: 'nave',
     setScale: { x: 0.06, y: 0.06 },
-    setOrigin: { x: 0, y: 0 }
+    setOrigin: { x: 0, y: 0 },
+    "setDepth.value": 60
   })
 
   nave.body.setMaxSpeed(250)
