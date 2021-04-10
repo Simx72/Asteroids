ea2765cd7b32d200f69f096812ee57580c91e604
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-10 13:02:57 -0500

    add to project

--- ts/functions/create-nave.ts
+++ ts/functions/create-nave.ts
@@ -1,4 +1,5 @@
 import AsteroidsMainScene from '../scenes/game-scene';
+import { actualizarFondo } from './cargar-menu';
 export default function createNave(this: AsteroidsMainScene) {
   const nave = this.objeto(
     'nave',
@@ -16,6 +17,7 @@ export default function createNave(this: AsteroidsMainScene) {
 
   this.dato('intervalo puntos', window.setInterval(() => {
     this.dato('puntos', this.dato<number>('puntos') + 1)
+    actualizarFondo.bind(this)()
   }, 3000))
 
   vidas.createMultiple({
