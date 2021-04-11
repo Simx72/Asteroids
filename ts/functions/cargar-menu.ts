84c9621c3c9c2b66b6fe5c426aa138614a1744cb
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-11 15:25:27 -0500

    add to project

--- ts/functions/cargar-menu.ts
+++ ts/functions/cargar-menu.ts
@@ -2,33 +2,35 @@ import AsteroidsScene from "../scenes/templates/asteroids-scene";
 import { mostrarTexto } from './animateCSS';
 export function actualizarNivel(this: AsteroidsScene) {
   let puntos = this.dato('puntos')
-  if (puntos < 100) {
-    if (this.dato('nivel') != 1) {
-      this.dato('nivel', 1)
-      mostrarTexto('Nivel 1')
+  let canvas = document.querySelector('canvas')
+  if (canvas != null) {
+    if (puntos < 100) {
+      if (this.dato('nivel') != 1) {
+        mostrarTexto(`Nivel ${this.dato('nivel', 1)}`)
+      }
+      document.body.className = "level-1"
+      document.querySelector('canvas')?.classList.add('level-1')
+    } else if (puntos < 400) {
+      if (this.dato('nivel') != 2) {
+        this.dato('nivel', 2)
+        mostrarTexto('Nivel 2')
+      }
+      document.body.className = "level-2"
+      document.querySelector('canvas')?.classList.add('level-2')
+    } else if (puntos < 1000) {
+      if (this.dato('nivel') != 3) {
+        this.dato('nivel', 3)
+        mostrarTexto('Nivel 3')
+      }
+      document.body.className = "level-3"
+      document.querySelector('canvas')?.classList.add('level-3')
+    } else {
+      if (this.dato('nivel') != 4) {
+        this.dato('nivel', 4)
+        mostrarTexto('Nivel 4')
+      }
+      document.body.className = "level-4"
+      document.querySelector('canvas')?.classList.add('level-4')
     }
-    document.body.className = "level-1"
-    document.querySelector('canvas')?.classList.add('level-1')
-  } else if (puntos < 400) {
-    if (this.dato('nivel') != 2) {
-      this.dato('nivel', 2)
-      mostrarTexto('Nivel 2')
-    }
-    document.body.className = "level-2"
-    document.querySelector('canvas')?.classList.add('level-2')
-  } else if (puntos < 1000) {
-    if (this.dato('nivel') != 3) {
-      this.dato('nivel', 3)
-      mostrarTexto('Nivel 3')
-    }
-    document.body.className = "level-3"
-    document.querySelector('canvas')?.classList.add('level-3')
-  } else {
-    if (this.dato('nivel') != 4) {
-      this.dato('nivel', 4)
-      mostrarTexto('Nivel 4')
-    }
-    document.body.className = "level-4"
-    document.querySelector('canvas')?.classList.add('level-4')
   }
 }
