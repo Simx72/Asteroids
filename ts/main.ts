6f2ddcd16e357f4b6caae041da8877e5c7f26546
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-12 16:45:47 -0500

    add to project

--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,6 @@
 /// <reference path="../node_modules/phaser/types/phaser.d.ts"/>
 import Scenes from "./scenes";
-import { }, crearMenu } from './functions/cargar-menu';
+import { crearMenu } from './functions/cargar-menu';
 
 export var asteroidsPhaserGameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
