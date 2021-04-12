4068fe7824aadbc548e9b6baca38ccc79962fc87
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-11 21:09:55 -0500

    add to project

--- ts/functions/disparo.ts
+++ ts/functions/disparo.ts
@@ -24,7 +24,7 @@ export default function disparo(this: AsteroidsMainScene) {
 
   let audio = this.sound.add('audio.laser')
   audio.play('', {
-    volume: cookies.getNum('config-volumen-fx', true) / 100 * 0.1
+    volume: cookies.getNum('config-volumen-fx', true) / 100 * 0.2
   })
 
   if (this.physics.config.debug) {
