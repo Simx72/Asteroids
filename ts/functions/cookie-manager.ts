c06c9c4fa1906ab100308af79f9ad5ee62091d4b
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-11 20:04:00 -0500

    add to project

--- ts/functions/cookie-manager.ts
+++ ts/functions/cookie-manager.ts
@@ -1,5 +1,6 @@
+/* Based in w3 schoools */
 export namespace cookies {
-  function set(key: string, value: string, exdays:number = 30) {
+  /* function set(key: string, value: string, exdays: number = 30) {
     let d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
@@ -10,10 +11,10 @@ export namespace cookies {
     let ca = decodedCookie.split(';');
     return ca;
   }
-  function get(key: string) {
+  function findCookieVal(key: string) {
     let name = key + "=";
     let ca = getCookies()
-    for(let i = 0; i <ca.length; i++) {
+    for (let i = 0; i < ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) == ' ') {
         c = c.substring(1);
@@ -24,4 +25,26 @@ export namespace cookies {
     }
     return "";
   }
+  function get(key: string) {
+    let val = findCookieVal(key)
+    if (val.match(/^[\{\[]/gm))
+      try {
+        return JSON.parse(val)
+      } catch (e) { "not a json" }
+    
+    if (val.match(/[\d\.]/gm))
+      try {
+        return JSON.parse(val)
+      } catch (e) { "not a json" }
+    
+    return val;
+  } */
+
+  function set(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
 }
