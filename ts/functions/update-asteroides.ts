d5c88f0b4424a14e174df79cb39f27e2336787df
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-13 07:48:54 -0500

    add to project

--- ts/functions/update-asteroides.ts
+++ ts/functions/update-asteroides.ts
@@ -1,4 +1,5 @@
 import AsteroidsMainScene from '../scenes/game-scene';
+import { cookies } from './cookie-manager';
 import { nuevoAsteroide } from './create-asteroides';
 export function updateAsteroides(this: AsteroidsMainScene) {
   let asteroides = <Phaser.Physics.Arcade.Group>this.objeto('grupo.ast')
@@ -9,24 +10,27 @@ export function updateAsteroides(this: AsteroidsMainScene) {
     if ((-ast.displayWidth / 2) > ast.x && (ast.displayWidth / 2 + this.scale.width) < ast.x && (-ast.displayHeight / 2) > ast.y && (ast.displayHeight / 2 + this.scale.height) < ast.y) ast.destroy()
   })
 
-  let densidad = 1;
-  switch (this.dato('nivel')) {
-    case 1:
-      densidad = 4
-      break;
-      case 2:
-      densidad = 3
-      break;
-      case 3:
-      densidad = 2
-      break;
-      case 4:
-      densidad = 1
-      break;
-  }
+  cookies.check('pausa')
+    .catch(() => {
+      let densidad = 1;
+      switch (this.dato('nivel')) {
+        case 1:
+          densidad = 4
+          break;
+        case 2:
+          densidad = 3
+          break;
+        case 3:
+          densidad = 2
+          break;
+        case 4:
+          densidad = 1
+          break;
+      }
 
-  if (this.game.getFrame() % densidad == 0 && this.dato('cargado') && !this.physics.config.debug) {
-    nuevoAsteroide.bind(this)()
-  }
+      if (this.game.getFrame() % densidad == 0 && this.dato('cargado') && !this.physics.config.debug) {
+        nuevoAsteroide.bind(this)()
+      }
+    })
 
 }
