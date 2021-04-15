218ab05e9a02aa18f320fafba6d1e2e17a286d6b
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-15 17:31:47 -0500

    add to project

--- ts/functions/audio.ts
+++ ts/functions/audio.ts
@@ -3,6 +3,7 @@ import { cookies } from "./cookie-manager";
 let musica = document.createElement('audio')
 document.body.appendChild(musica)
 musica.id = 'elemento-musica'
+musica.volume = cookies.getNum('config-volume-musica', true)
 
 document.getElementById('volumen-efectos')
   ?.addEventListener('change', e => {
@@ -13,7 +14,7 @@ document.getElementById('volumen-efectos')
 musica.addEventListener('change', e => {
   let element = e.target as HTMLInputElement;
   cookies.set('config-volumen-musica', element.value.toString())
-  musica.volume = parseInt(element.value)/100
+  musica.volume = parseInt(element.value) / 100
 })
 
 export default musica;
