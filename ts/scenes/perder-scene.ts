22892f22f7728c05e91dc998372f36b388e03fbe
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-17 15:08:26 -0500

    add to project

--- ts/scenes/perder-scene.ts
+++ ts/scenes/perder-scene.ts
@@ -12,79 +12,82 @@ export default class AsteroidsLooseScene extends AsteroidsScene {
 
   preload() {
     try {
-    this.defaultPreload()
-  } catch (e) {
-    sendError(e, this.physics.config.debug)
-  }
+      this.defaultPreload()
+    } catch (e) {
+      sendError(e, this.physics.config.debug)
+    }
   }
 
   create() {
     try {
-    document.body.classList.toString().split(' ').forEach(val => document.body.classList.remove(val))
-    document.body.classList.add('loose')
-    this.dato('puntos', cookies.getNum('puntos', true))
-    cookies.remove('puntos')
-    cargarMenu.bind(this)()
+      document.body.classList.toString().split(' ').forEach(val => document.body.classList.remove(val))
+      document.body.classList.add('loose')
+      this.dato('puntos', cookies.getNum('puntos', true))
+      cookies.remove('puntos')
+      cargarMenu.bind(this)()
 
-    if (this.physics.config.debug) {
-      this.objeto(
-        'texto.debug',
-        this.add.text(0, 0, `[scene]: Perder Scene (${this.scene.key}) \n\nPuntos: ${this.dato<number>('puntos')}\n`).setOrigin(0, 0).setPosition(10, 10).setDepth(100)
-      );
-    } else {
-      this.objeto(
-        'texto.puntos',
-        this.add.text(0, 0, 'Tu puntaje: ' + this.dato<number>('puntos'), { fontFamily: this.defaultFont, fontSize: '25pt' }).setOrigin(0, 0).setPosition(10, 10).setDepth(70)
-      )
+      if (this.physics.config.debug) {
+        this.objeto(
+          'texto.debug',
+          this.add.text(0, 0, `[scene]: Perder Scene (${this.scene.key}) \n\nPuntos: ${this.dato<number>('puntos')}\n`).setOrigin(0, 0).setPosition(10, 10).setDepth(100)
+        );
+      } else {
+        this.objeto(
+          'texto.puntos',
+          this.add.text(0, 0, 'Tu puntaje: ' + this.dato<number>('puntos'), { fontFamily: this.defaultFont, fontSize: '25pt' }).setOrigin(0, 0).setPosition(10, 10).setDepth(70)
+        )
+      }
+
+      let texto = this.add.text(
+        this.centerX,
+        this.centerY,
+        'Perdiste!',
+        { fontFamily: this.defaultFont }
+      ).setOrigin(0.5, 0.5)
+        .setFontSize(this.scale.width * 0.08)
+        .setColor('#e40F0F')
+
+      let button = this.add.rectangle(
+        this.centerX,
+        texto.y + texto.displayHeight / 2 + 10,
+        this.scale.width * 0.3,
+        this.scale.height * 0.13,
+        0xFFFFFF
+      ).setOrigin(0.5, 0).setInteractive()
+
+      let buttonCenter = button.getCenter()
+
+      let buttonTexto = this.add.text(
+        buttonCenter.x,
+        buttonCenter.y,
+        'Reiniciar',
+        { fontFamily: this.defaultFont }
+      ).setOrigin(0.5, 0.5)
+
+      buttonTexto.setFontSize(this.scale.width * 0.04)
+      buttonTexto.setColor('#000000')
+
+      button.on('pointerdown', () => {
+        if (cookies.get('jugando') == "true") {
+          button.setFillStyle(0xFFFFFF)
+          this.game.scene.stop(this).run('game-scene')
+          this.game.canvas.style.cursor = 'default'
+        }
+      })
+      button.on('pointerover', () => {
+        if (cookies.get('jugando') == "true") {
+          this.game.canvas.style.cursor = 'pointer'
+          button.setFillStyle(0xDDDD33)
+        }
+      })
+      button.on('pointerout', () => {
+        if (cookies.get('jugando') == "true") {
+          this.game.canvas.style.cursor = 'default'
+          button.setFillStyle(0xFFFFFF)
+        }
+      })
+    } catch (e) {
+      sendError(e, this.physics.config.debug)
     }
-
-    let texto = this.add.text(
-      this.centerX,
-      this.centerY,
-      'Perdiste!',
-      { fontFamily: this.defaultFont }
-    ).setOrigin(0.5, 0.5)
-      .setFontSize(this.scale.width * 0.08)
-      .setColor('#e40F0F')
-
-    let button = this.add.rectangle(
-      this.centerX,
-      texto.y + texto.displayHeight / 2 + 10,
-      this.scale.width * 0.3,
-      this.scale.height * 0.13,
-      0xFFFFFF
-    ).setOrigin(0.5, 0).setInteractive()
-
-    let buttonCenter = button.getCenter()
-
-    let buttonTexto = this.add.text(
-      buttonCenter.x,
-      buttonCenter.y,
-      'Reiniciar',
-      { fontFamily: this.defaultFont }
-    ).setOrigin(0.5, 0.5)
-
-    buttonTexto.setFontSize(this.scale.width * 0.04)
-    buttonTexto.setColor('#000000')
-
-    button.on('pointerdown', () => {
-      if (cookies.get('jugando') == "true") {
-        button.setFillStyle(0xFFFFFF)
-        this.game.scene.stop(this).run('game-scene')
-        this.game.canvas.style.cursor = 'default'
-      }
-    })
-    button.on('pointerover', () => {
-      if (cookies.get('jugando') == "true") {
-        this.game.canvas.style.cursor = 'pointer'
-        button.setFillStyle(0xDDDD33)
-      }
-    })
-    button.on('pointerout', () => {
-      if (cookies.get('jugando') == "true") {
-        this.game.canvas.style.cursor = 'default'
-        button.setFillStyle(0xFFFFFF)
-      }
-    })
   }
 }
