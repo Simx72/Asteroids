5ae2487217cc5932f3bfd37570fce0978fa22a28
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-17 15:56:43 -0500

    add to project

--- ts/functions/audio.ts
+++ ts/functions/audio.ts
@@ -5,7 +5,7 @@ document.body.appendChild(musica)
 musica.id = 'elemento-musica'
 musica.src = 'https://pagina-simx72-aba9b.web.app/asteroids-assets/sounds/Crystal-Cave-Song-18.mp3'
 console.log(cookies.getNum('config-volumen-musica', true) / 100)
-musica.volume = cookies.getNum('config-volume-musica', true) / 100
+musica.volume = cookies.getNum('config-volumen-musica', true) / 100
 musica.play()
 
 
