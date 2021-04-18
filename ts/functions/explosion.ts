e09becc169c84e2873f9d338e378993321a5c171
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-18 13:40:35 -0500

    add to project

--- ts/functions/explosion.ts
+++ ts/functions/explosion.ts
@@ -1,4 +1,23 @@
 import AsteroidsMainScene from '../scenes/game-scene';
-function explosion(this: AsteroidsMainScene) {
-  
+export function explosion(this: AsteroidsMainScene, x: number, y: number) {
+  let exp = this.add.sprite(x, y, 'explosion').setVisible(false)
+
+  exp.anims.create({
+    key: 'explotar',
+    frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 15 }),
+    frameRate: 40,
+    repeat: 0
+  })
+
+  return new Promise<Phaser.GameObjects.Sprite>((resolve, reject) => {
+    exp
+    .setPosition(x, y)
+    .setVisible(true)
+      .once('animationcomplete', () => {
+        resolve(exp)
+        exp.destroy()
+      })
+    .anims.play('explotar')
+  })
+
 }
