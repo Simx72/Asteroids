e36173190a8779ac863fdbf918858145f36b1343
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-18 13:25:13 -0500

    add to project

--- ts/functions/cargar-menu.ts
+++ ts/functions/cargar-menu.ts
@@ -110,10 +110,16 @@ export function cargarMenu(this: AsteroidsScene) {
     if (jugando == "true") {
       menuclick.once("click", () => {
         this.physics.pause()
+        menuclick.once("click", () => {
+          this.physics.resume()
+        })
       })
     } else if (jugando == "false") {
       menuclick.once("click", () => {
         this.physics.resume()
+        menuclick.once("click", () => {
+          this.physics.pause()
+        })
       })
     }
   }
