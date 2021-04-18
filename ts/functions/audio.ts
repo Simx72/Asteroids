dccfae0748323dbe27a2f2990815032f2314bd52
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-17 19:52:32 -0500

    add to project

--- ts/functions/audio.ts
+++ ts/functions/audio.ts
@@ -8,33 +8,35 @@ musica.volume = cookies.getNum('config-volumen-musica', true) / 100
 musica.loop = true
 musica.play()
 
-let inputEfectos = document.querySelector<HTMLInputElement>('#volumen-efectos')
-if (inputEfectos != null) {
+document.addEventListener('DOMContentLoaded', () => {
 
-  inputEfectos.value = cookies.get('config-volumen-fx')
-  
-  inputEfectos.addEventListener('change', e => {
-    e.preventDefault()
-    if (inputEfectos != null) {
-      cookies.set('config-volumen-fx', inputEfectos.value)
-    }
-  })
-}
+  let inputEfectos = document.querySelector<HTMLInputElement>('#volumen-efectos')
+  if (inputEfectos != null) {
 
-let inputMusica = document.querySelector<HTMLInputElement>('#volumen-musica')
-if (inputMusica != null) {
-  
-inputMusica.value = cookies.get('config-volumen-musica')
-  
-  inputMusica.addEventListener('change', e => {
-    e.preventDefault()
-    if (inputMusica != null) {
-      cookies.set('config-volumen-musica', inputMusica.value.toString())
-      musica.volume = parseInt(inputMusica.value) / 100
-    }
-  })
-}
+    inputEfectos.value = cookies.get('config-volumen-fx')
 
-console.log(inputEfectos, inputMusica)
+    inputEfectos.addEventListener('change', e => {
+      e.preventDefault()
+      if (inputEfectos != null) {
+        cookies.set('config-volumen-fx', inputEfectos.value)
+      }
+    })
+  }
+
+  let inputMusica = document.querySelector<HTMLInputElement>('#volumen-musica')
+  if (inputMusica != null) {
+
+    inputMusica.value = cookies.get('config-volumen-musica')
+
+    inputMusica.addEventListener('change', e => {
+      e.preventDefault()
+      if (inputMusica != null) {
+        cookies.set('config-volumen-musica', inputMusica.value.toString())
+        musica.volume = parseInt(inputMusica.value) / 100
+      }
+    })
+  }
+
+}, false)
 
 export default musica;
