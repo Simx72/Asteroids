22138c3949224e56e6c4d4a36a83212be80f6d8d
Simx72 <53983258+Simx72@users.noreply.github.com>
2022-02-04 19:05:02 -0500

    add parent container to core

--- src/core.ts
+++ src/core.ts
@@ -2,6 +2,9 @@ import * as Phaser from 'phaser';
 import FirstScene from "./scenes/game-scene";
 import './functions/audio'
 
+const parent = document.createElement("main");
+document.body.appendChild(parent);
+
 export var asteroidsGameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   scale: {
@@ -11,6 +14,10 @@ export var asteroidsGameConfig: Phaser.Types.Core.GameConfig = {
   scene: FirstScene,
   backgroundColor: '#00000000',
   transparent: true,
+  dom: {
+    createContainer: true
+  },
+  parent,
   physics: {
     default: 'arcade',
     arcade: {
