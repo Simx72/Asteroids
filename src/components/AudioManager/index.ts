e4af026ddea3e0e5d9ca6d15eb2db7a100ed6b63
Simx72 <53983258+Simx72@users.noreply.github.com>
2022-02-05 19:29:43 -0500

    selete musica.ts file and add code to AudioManager

--- src/components/AudioManager/index.ts
+++ src/components/AudioManager/index.ts
@@ -1,45 +1,47 @@
-import musica from '../../musica';
 import Scene from '../../scenes/templates/asteroids-scene';
 import cookies from '../../cookies';
+
 export default class AudioManager extends Phaser.GameObjects.GameObject {
   constructor(scene: Scene) {
     super(scene, "manager")
 
+    document.body.appendChild(this.musica)
+    this.musica.loop = true
 
-    let inputEfectos = document.querySelector<HTMLInputElement>('#volumen-efectos')
-    if (inputEfectos != null) {
-
-      inputEfectos.value = cookies.get('config-volumen-fx')
-
-      inputEfectos.addEventListener('change', e => {
-        if (inputEfectos != null) {
-          cookies.set('config-volumen-fx', inputEfectos.value)
-        }
-      })
-    }
-
-    let inputMusica = document.querySelector<HTMLInputElement>('#volumen-musica')
-    if (inputMusica != null) {
-
-      inputMusica.value = cookies.get('config-volumen-musica')
-
-      inputMusica.addEventListener('change', e => {
-        if (inputMusica != null) {
-          cookies.set('config-volumen-musica', inputMusica.value)
-          musica.volume = parseInt(inputMusica.value) / 100
-        }
-      })
-    }
+    this.musicVolume = Math.floor(cookies.getNum('config-volumen-musica', true)) / 100
+    this.musicVolume = Math.floor(cookies.getNum('config-volumen-fx', true)) / 100
 
   }
 
-  currentSong = ""
-  musica = musica
+  musica = document.createElement('audio')
+
+  protected currentSong = ""
+  private _volume = {
+    music: 0,
+    fx: 0
+  }
+
+  get musicVolume(): number {
+    return this._volume.music;
+  }
+  set musicVolume(val: number) {
+    this._volume.music = val;
+    this.musica.volume = val;
+    cookies.set('config-volumen-musica', val.toString());
+  }
+
+  get fxVolume(): number {
+    return this._volume.fx;
+  }
+  set fxVolume(val: number) {
+    this._volume.fx;
+    cookies.set('config-volumen-fx', val.toString())
+  }
 
   setSong(url: string) {
     if (this.currentSong != url) {
-      musica.src = url
-      this.currentSong = url
+      this.musica.src = url;
+      this.currentSong = url;
     }
   }
 
--- src/musica.ts
+++
@@ -1,8 +0,0 @@
-import cookies from './cookies';
-
-let musica = document.createElement('audio')
-document.body.appendChild(musica)
-musica.volume = Math.floor(cookies.getNum('config-volumen-musica', true)) / 100
-musica.loop = true
-
-export default musica;
