926449d4c2acd9e88cb96a989249128a1f31f466
Simx72 <53983258+Simx72@users.noreply.github.com>
2022-02-06 21:29:28 -0500

    add play and pause to Pausa

--- src/components/Pausa/index.ts
+++ src/components/Pausa/index.ts
@@ -2,8 +2,8 @@ import Scene from '../../scenes/templates/default';
 import asset_htmlpausa from "./menu-pausa.html";
 import styles from "./styles.module.css";
 
-export default class Pausa extends Phaser.GameObjects.DOMElement {
-  constructor (scene: Scene) {
+class Pausa extends Phaser.GameObjects.DOMElement {
+  constructor(scene: Scene) {
     super(scene, 0, 0, "div");
 
     this.createFromHTML(asset_htmlpausa)
@@ -11,8 +11,31 @@ export default class Pausa extends Phaser.GameObjects.DOMElement {
     this.setClassName(styles["menu-pausa"])
 
 
+
+
+  }
+
+  pause() {
+    this.node.style.display = "flex!important";
+    this.emit(Pausa.Events.Music.Pause)
+  }
+  
+  play() {
+    this.node.style.display = "none";
+    this.emit(Pausa.Events.Music.Play)
   }
 
   node!: HTMLDivElement;
 
 }
+
+namespace Pausa {
+  export namespace Events {
+    export namespace Music {
+      export var Pause = 'musicpause';
+      export var Play = 'musicplay';
+    }
+  }
+}
+
+export default Pausa;
